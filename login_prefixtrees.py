6462980495c84959f20f24255f490eb8135281b7
Florian <florian.fogliani@gmail.com>
2023-10-07 11:43:30 +0200

    Level 1 : all. Level 2 : all except SearchWord and Hangman.

--- login_prefixtrees.py
+++ login_prefixtrees.py
@@ -21,18 +21,40 @@ def countwords(T):
     """ count words in the prefix tree T (ptree.Tree)
     return type: int
     """
-    
-    #FIXME
-    pass
+    res=0
+    if (T.key[1]) :
+        res = 1
+    for c in T.children :
+        res += countwords(c)
+    return res
     
 
 def averagelength(T):
     """ average word length in the prefix tree T (ptree.Tree)
     return type: float
     """
-    
-    #FIXME
-    pass
+    res=0
+    nb_word=0
+    for c in T.children :
+        (len,nb) = __average__(c,1)
+        res += len
+        nb_word += nb
+    return res/nb_word if nb_word != 0 else 0
+
+def __average__(T,level):
+    """ average depend on the level
+    return type : tuple
+    """
+    res=0
+    nb_word = 0
+    if (T.key[1]):
+        res += level
+        nb_word+=1
+    for c in T.children :
+        (len,nb) = __average__(c,level+1)
+        res += len
+        nb_word += nb
+    return (res,nb_word)
     
 
 ###############################################################################
@@ -42,27 +64,45 @@ def wordlist(T):
     """ generate the word list, in alphabetic order, of the prefix tree T (ptree.Tree)
     return type: str list
     """
-    
-    #FIXME
-    pass
+    res = []
+    if (T.key[1]):
+        res += [[T.key[0]]]
+    for c in T.children : 
+        for w in wordlist(c):
+            word = T.key[0]
+            for l in w:
+                word+= l
+            res += [word]
+                   
+    return res        
 
 
 def longestword(T):
     """ search for the longest word in the prefix tree T (ptree.Tree)
     return type: str    
     """
+    words = wordlist(T)
+    if (len(words) == 0):
+        return ""
+    return max(words,key=len)
     
-    #FIXME
-    pass
 
 
 def searchword(T, w):
     """ search for the word w (str) not empty in the prefix tree T (ptree.Tree)
     return type: bool
     """
-    
-    #FIXME
-    pass
+    if (T.key[0] == '') :
+        for c in T.children : 
+            if (searchword(c,w)):
+                return True
+    else :
+        if (len(w) == 1 and T.key[0]==w):
+            return True
+        for c in T.children :
+            if (searchword(c,w[1:])):
+                return True
+    return False
     
 
 def hangman(T, pattern):
---
+++ test.py
@@ -0,0 +1,4 @@
+import login_prefixtrees
+import prefixtreesexample
+
+print(login_prefixtrees.searchword(prefixtreesexample.Tree1,"cou"))
